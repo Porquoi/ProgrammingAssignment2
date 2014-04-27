3e33aebc05b9561f7a6b9b9f58034681715598ef
Porquoi <UlyssesNicOwen@gmail.com>
2014-04-27 05:01:40 -0400

    Commiting new functions makeCacheMatrix & cacheSolve.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates an object with four function methods that construct and retrieve the matrix
+## itself and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     inv <- matrix() #create a 1x1 matrix containing a value to hold the inverse
+     set <- function(y = matrix()){ # create a function, set, to eventually the input as its value
+       x <<- y
+       inv <<- matrix() # re-initialize the inverse holder to a "blank"
+     }
+     get <- function() x # output whatever matrix we stored originally
+     setinv <- function(inverse) inv <<- inverse # if we calculated some inverse set inv 
+     # equal to it
+     getinv <- function() inv # output the value of whatever inverse we've computed
+     list(set = set, #create a list with the different functions
+          get = get,
+          setinv = setinv,
+          getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of cacheMatrix object if inv not initialized, otherwise
+## outputs cached object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
+  inv <- x$getinv() #output the inverse that we stored
+  if(sum(dim(inv))== 2 && all(is.na(matrix()))){ #if the matrix has exactly dimesion one
+    #and is composed of a single NA
+    operand <- x$get() #get the operand, our matrix, out of the list
+    inv <- solve(operand) # find its inverse
+    x$setinv(inv) #set its inverse
+    inv #print that inverse
+  } 
+  else{
+    message("Returning cached inverse") #otherwise print message
+    return(inv) # and return the previously calculated, cached inverse
+  }
+  
+    
 }
